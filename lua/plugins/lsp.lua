53deaab5a7f6dc532a01a4ab0d5f3982fae772a0
emmanuel <emmlop243@gmail.com>
2026-03-21 18:42:30 -0600

    rename file to `lsp.lua` and added lsp for vue-ts

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -7,6 +7,7 @@ return {
     "mason-org/mason-lspconfig.nvim",
     opts = {
       ensure_installed = {
+        "vtsls",      -- Vue/TS
         "ts_ls",     -- JS/TS
         "vue_ls",        -- Vue
         "lua_ls",       -- Neovim config
