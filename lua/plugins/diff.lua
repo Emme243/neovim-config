fc1d1d1b70db4da0d40a8da67ea497dd3d54152a
emmanuel <emmlop243@gmail.com>
2026-03-22 16:36:17 -0600

    added keybindings for mini.diff

--- lua/plugins/diff.lua
+++ lua/plugins/diff.lua
@@ -10,13 +10,36 @@ return {
 			end,
 			desc = "Toggle diff overlay",
 		},
+		{
+			"<leader>dr",
+			function()
+				MiniDiff.do_hunks(0, "reset", { line_start = vim.fn.line("."), line_end = vim.fn.line(".") })
+			end,
+			desc = "Reset diff (line)",
+		},
+		{
+			"<leader>dR",
+			function()
+				MiniDiff.do_hunks(0, "reset", { line_start = 1, line_end = vim.api.nvim_buf_line_count(0) })
+			end,
+			desc = "Reset diff (file)",
+		},
+		{
+			"]h",
+			function()
+				MiniDiff.goto_hunk("next")
+			end,
+			desc = "Next diff hunk",
+		},
+		{
+			"[h",
+			function()
+				MiniDiff.goto_hunk("prev")
+			end,
+			desc = "Prev diff hunk",
+		},
 	},
 	config = function()
-		require("mini.diff").setup({
-			view = {
-				style = "sign",
-				signs = { add = "▎", change = "▎", delete = "" },
-			},
-		})
+		require("mini.diff").setup()
 	end,
 }
