1b637cf5265c5e22165371c935478f0a03fc70dc
emmanuel <emmlop243@gmail.com>
2026-03-21 18:26:49 -0600

    added lsp config

---
+++ lua/plugins/mason.lua
@@ -0,0 +1,24 @@
+return {
+  {
+    "mason-org/mason.nvim",
+    opts = {}
+  },
+  {
+    "mason-org/mason-lspconfig.nvim",
+    opts = {
+      ensure_installed = {
+        "ts_ls",     -- JS/TS
+        "vue_ls",        -- Vue
+        "lua_ls",       -- Neovim config
+        "pyright",      -- Python
+        "jsonls",       -- JSON
+        "html",         -- HTML
+        "cssls",        -- CSS
+      }
+    },
+    dependencies = {
+      { "mason-org/mason.nvim", opts = {} },
+      "neovim/nvim-lspconfig",
+    },
+  }
+} 
