40f76b7123979b827ff3a30bf54a5b5333b64279
emmanuel <emmlop243@gmail.com>
2026-03-21 19:17:08 -0600

    make sure vuets lsp works

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,25 +1,79 @@
 return {
   {
     "mason-org/mason.nvim",
-    opts = {}
+    config = function()
+      require("mason").setup()
+    end,
   },
+
   {
     "mason-org/mason-lspconfig.nvim",
-    opts = {
-      ensure_installed = {
-        "vtsls",      -- Vue/TS
-        "ts_ls",     -- JS/TS
-        "vue_ls",        -- Vue
-        "lua_ls",       -- Neovim config
-        "pyright",      -- Python
-        "jsonls",       -- JSON
-        "html",         -- HTML
-        "cssls",        -- CSS
-      }
-    },
     dependencies = {
-      { "mason-org/mason.nvim", opts = {} },
+      "mason-org/mason.nvim",
       "neovim/nvim-lspconfig",
     },
-  }
-} 
+    config = function()
+      require("mason-lspconfig").setup({
+        ensure_installed = {
+          "vtsls",
+          "vue_ls",
+          "lua_ls",
+          "pyright",
+          "jsonls",
+          "html",
+          "cssls",
+        },
+      })
+    end,
+  },
+
+  {
+    "neovim/nvim-lspconfig",
+    dependencies = {
+      "mason-org/mason.nvim",
+      "mason-org/mason-lspconfig.nvim",
+    },
+    config = function()
+      local lspconfig = require("lspconfig")
+
+      local vue_language_server_path =
+          vim.fn.stdpath("data") .. "/mason/packages/vue-language-server/node_modules/@vue/language-server"
+
+      local vue_plugin = {
+        name = "@vue/typescript-plugin",
+        location = vue_language_server_path,
+        languages = { "vue" },
+        configNamespace = "typescript",
+      }
+
+      lspconfig.vtsls.setup({
+        filetypes = {
+          "javascript",
+          "javascriptreact",
+          "javascript.jsx",
+          "typescript",
+          "typescriptreact",
+          "typescript.tsx",
+          "vue",
+        },
+        settings = {
+          vtsls = {
+            tsserver = {
+              globalPlugins = {
+                vue_plugin,
+              },
+            },
+          },
+        },
+      })
+
+      lspconfig.vue_ls.setup({})
+
+      lspconfig.lua_ls.setup({})
+      lspconfig.pyright.setup({})
+      lspconfig.jsonls.setup({})
+      lspconfig.html.setup({})
+      lspconfig.cssls.setup({})
+    end,
+  },
+}
