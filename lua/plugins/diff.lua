4e0b0b7ea3444bfe316195916e58536b232bec8e
emmanuel <emmlop243@gmail.com>
2026-03-23 18:54:03 -0600

    improved mini.diff keybindings

--- lua/plugins/diff.lua
+++ lua/plugins/diff.lua
@@ -4,21 +4,21 @@ return {
 	event = "VeryLazy",
 	keys = {
 		{
-			"<leader>do",
+			"<leader>ho",
 			function()
 				MiniDiff.toggle_overlay(0)
 			end,
 			desc = "Toggle diff overlay",
 		},
 		{
-			"<leader>dr",
+			"<leader>hr",
 			function()
 				MiniDiff.do_hunks(0, "reset", { line_start = vim.fn.line("."), line_end = vim.fn.line(".") })
 			end,
 			desc = "Reset diff (line)",
 		},
 		{
-			"<leader>dR",
+			"<leader>hR",
 			function()
 				MiniDiff.do_hunks(0, "reset", { line_start = 1, line_end = vim.api.nvim_buf_line_count(0) })
 			end,
@@ -40,6 +40,11 @@ return {
 		},
 	},
 	config = function()
-		require("mini.diff").setup()
+		require("mini.diff").setup({
+			view = {
+				style = "sign",
+				signs = { add = "▎", change = "▎", delete = "" },
+			},
+		})
 	end,
 }
